c6886fab3fab2a595776acd584b2ec4cdc5c8abb
moralok <moralok@163.com>
2021-02-28 00:34:58 +0800

    线程中断
    许多方法抛出InterruptedException之前会将中断标识位清除

---
+++ src/main/java/com/moralok/ch4/Interrupted.java
@@ -0,0 +1,51 @@
+package com.moralok.ch4;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author moralok
+ * @since 2021/2/28
+ */
+public class Interrupted {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread sleepThread = new Thread(new SleepRunner(), "SleepRunner");
+        sleepThread.setDaemon(true);
+        Thread busyThread = new Thread(new BusyRunner(), "BusyRunner");
+        busyThread.setDaemon(true);
+        sleepThread.start();
+        busyThread.start();
+        TimeUnit.SECONDS.sleep(5);
+        sleepThread.interrupt();
+        busyThread.interrupt();
+        // false and true
+        // 许多方法抛出InterruptedException之前会将中断标识位清除
+        System.out.println("SleepThread interrupted is " + sleepThread.isInterrupted());
+        System.out.println("BusyThread interrupted is " + busyThread.isInterrupted());
+        TimeUnit.SECONDS.sleep(2);
+    }
+
+    static class SleepRunner implements Runnable {
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    TimeUnit.SECONDS.sleep(10);
+                } catch (InterruptedException e) {
+
+                }
+            }
+        }
+    }
+
+    static class BusyRunner implements Runnable {
+
+        @Override
+        public void run() {
+            while (true) {
+
+            }
+        }
+    }
+}
