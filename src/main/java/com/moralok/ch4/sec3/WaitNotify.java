ca66f3dfbf54727fc371262a0557586b006ef8d8
moralok <moralok@163.com>
2021-02-28 21:15:34 +0800

    线程的等待、通知机制
    wait、notify

---
+++ src/main/java/com/moralok/ch4/sec3/WaitNotify.java
@@ -0,0 +1,78 @@
+package com.moralok.ch4.sec3;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 3、4顺序可能变动
+ * Thread[WaitThread,5,main] flag is true. wait @ 21:14:25
+ * Thread[NotifyThread,5,main] hold lock. notify @ 21:14:26
+ * Thread[WaitThread,5,main] flag is false. running @ 21:14:31
+ * Thread[NotifyThread,5,main] hold lock again. sleep @ 21:14:31
+ *
+ * @author moralok
+ * @since 2021/2/28
+ */
+public class WaitNotify {
+
+    private static boolean flag = true;
+    private static Object lock = new Object();
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread waitThread = new Thread(new Wait(), "WaitThread");
+        waitThread.start();
+        TimeUnit.SECONDS.sleep(1);
+        Thread notifyThread = new Thread(new Notify(), "NotifyThread");
+        notifyThread.start();
+    }
+
+    static class Wait implements Runnable {
+
+        @Override
+        public void run() {
+            // 加锁，拥有lock的Monitor
+            synchronized (lock) {
+                // 当条件不满足时，继续wait，同时释放了lock的锁
+                while (flag) {
+                    try {
+                        System.out.println(Thread.currentThread() + " flag is true. wait @ " + new SimpleDateFormat("HH:mm:ss").format(new Date()));
+                        lock.wait();
+                    } catch (InterruptedException e) {
+
+                    }
+                }
+                // 条件满足时，完成工作
+                System.out.println(Thread.currentThread() + " flag is false. running @ " + new SimpleDateFormat("HH:mm:ss").format(new Date()));
+            }
+        }
+    }
+
+    static class Notify implements Runnable {
+
+        @Override
+        public void run() {
+            // 加锁，拥有lock的Monitor
+            synchronized (lock) {
+                // 获取lock的锁，然后进行通知，通知时不会释放lock的锁
+                // 直到当前线程释放了lock后，WaitThread才能从wait方法中返回
+                System.out.println(Thread.currentThread() + " hold lock. notify @ " + new SimpleDateFormat("HH:mm:ss").format(new Date()));
+                lock.notifyAll();
+                flag = false;
+                try {
+                    TimeUnit.SECONDS.sleep(5);
+                } catch (InterruptedException e) {
+
+                }
+            }
+            synchronized (lock) {
+                System.out.println(Thread.currentThread() + " hold lock again. sleep @ " + new SimpleDateFormat("HH:mm:ss").format(new Date()));
+                try {
+                    TimeUnit.SECONDS.sleep(5);
+                } catch (InterruptedException e) {
+
+                }
+            }
+        }
+    }
+}
