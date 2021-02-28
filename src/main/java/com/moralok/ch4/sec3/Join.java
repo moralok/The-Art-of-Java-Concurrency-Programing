b77d85cbbbf289dc82e4f60e9acdbc91de5665c8
moralok <moralok@163.com>
2021-02-28 21:34:58 +0800

    Thread.join

---
+++ src/main/java/com/moralok/ch4/sec3/Join.java
@@ -0,0 +1,41 @@
+package com.moralok.ch4.sec3;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author moralok
+ * @since 2021/2/28
+ */
+public class Join {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread previous = Thread.currentThread();
+        int count = 10;
+        for (int i = 0; i < count; i++) {
+            Thread thread = new Thread(new Domino(previous), String.valueOf(i));
+            thread.start();
+            previous = thread;
+        }
+        TimeUnit.SECONDS.sleep(5);
+        System.out.println(Thread.currentThread().getName() + " terminate.");
+    }
+
+    static class Domino implements Runnable {
+
+        private Thread thread;
+
+        public Domino(Thread thread) {
+            this.thread = thread;
+        }
+
+        @Override
+        public void run() {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + " terminate.");
+        }
+    }
+}
